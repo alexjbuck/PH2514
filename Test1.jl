536eae3d260d9dbbe801d27bb0c6b66e5484115f
Alex Buck <alexjbuck@gmail.com>
2020-07-18 09:08:05 -0700

    Adding functions

---
+++ Test1.jl
@@ -0,0 +1,14 @@
+using PH2514
+
+# Question 1
+# What is the Energy (in electron Volts - eV) associated with radiation at 13 micrometers? (Do not include units with your answer)
+E_hf(f_cλ(13e-6),:eV)
+
+
+# Question 2
+# Transitions to the n = 5 state of the hydrogen atom are known as the
+# Pfund
+
+# Question 3
+# What is the ground state energy for a Lithium ion, in eV? (Note that Z=3, the ground state energy for hydrogen is -13.58 eV). Enter the absolute value of your answer, i.e. don't enter the minus sign or list any units.
+E_Zn(3,1)
--- src/PH2514.jl
+++ src/PH2514.jl
@@ -1,5 +1,31 @@
 module PH2514
 
 # Write your package code here.
+export C, λ_cf, f_cλ, E_hf, E_Zn
 
+C = Dict(
+    :c => 3e8,
+    :h => 6.626e-34,
+    :J2eV => 6.242e18,
+    :E1 => -13.58
+)
+
+function λ_cf(f)
+    C[:c] * f end
+end
+function f_cλ(λ)
+    C[:c] / λ
+end
+function E_hf(f)
+    C[:h] * f
+end
+function E_hf(f,unit::Symbol)
+    if unit == :J
+        E_hf(f)
+    elseif unit == :eV
+        E_hf(f) * C[:J2eV]
+    end
+end
+function E_Zn(Z,n)
+    C[:E1] * (Z/n)^2
 end
